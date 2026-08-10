90f30f8d9189965e070ff0b2c8478c79edb7425d
SynapticFour <SynapticFour@users.noreply.github.com>
2026-08-10 02:54:40 +0000

    deploy: 3ec40eb0654a592d07011288e99b8c5f993f887d

--- nightly/gatk_haplotypecaller/shared_bam/sidebar-items.js
+++ nightly/gatk_haplotypecaller/shared_bam/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["empty_shared_record","into_unique_records","record_make_mut","share_record","share_records"],"trait":["BamRecordSlot"],"type":["SharedBamRecord"]};
+window.SIDEBAR_ITEMS = {"fn":["empty_shared_record","empty_shared_record_ref","into_unique_records","is_empty_shared_record","record_make_mut","share_record","share_records"],"trait":["BamRecordSlot"],"type":["SharedBamRecord"]};
